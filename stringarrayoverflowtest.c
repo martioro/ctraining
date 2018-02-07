5e89530aa9e54c192133f9b9e9bd294bf819d081
Martin Rødahl <martinrodahl@Martins-MacBook-Pro-2.local>
2018-02-07 21:32:10 +0100

    Baklengsprogram

---
+++ stringarrayoverflowtest.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+char input[20];
+
+int i=0;
+
+
+int main(void) {
+
+	printf("\nSkriv inn tekst under: ");
+	scanf("%20s", input);
+	printf("\n");
+
+	for(i=0;i<20;i++){
+
+		printf("%c",input[19-i]);
+
+	}
+
+	printf("\n");
+
+    return 0;
+}
+
